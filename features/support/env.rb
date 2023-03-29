9132e9044e58ea490b2d29a20a171b00f1c53036
Neha Agarwal <agarneha1331@gmail.com>
2023-03-29 13:57:00 +0530

    Parallel by default

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,43 +1,41 @@
+require 'yaml'
 require 'selenium/webdriver'
 require 'capybara/cucumber'
 require 'browserstack/local'
-require 'browserstack-automate'
-BrowserStack.for "cucumber"
 
-url = "https://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub.browserstack.com/wd/hub"
+# monkey patch to avoid reset sessions
+class Capybara::Selenium::Driver < Capybara::Driver::Base
+  def reset!
+    @browser.navigate.to('about:blank') if @browser
+  end
+end
+
+TASK_ID = (ENV['TASK_ID'] || 0).to_i
+CONFIG_NAME = ENV['CONFIG_NAME'] || 'single'
+
+CONFIG = YAML.safe_load(File.read(File.join(File.dirname(__FILE__), "../../config/#{CONFIG_NAME}.config.yml")))
+CONFIG['user'] = ENV['BROWSERSTACK_USERNAME'] || CONFIG['user']
+CONFIG['key'] = ENV['BROWSERSTACK_ACCESS_KEY'] || CONFIG['key']
 
 Capybara.register_driver :browserstack do |app|
-
-  capabilities = Selenium::WebDriver::Remote::Capabilities.new
-	if ENV['BS_AUTOMATE_OS']
-		capabilities['os'] = ENV['BS_AUTOMATE_OS']
-		capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION']
-	else
-		capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'ANY'
-	end
-
-	capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'chrome'
-	capabilities['browser_version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
-	capabilities['browserstack.debug'] = 'true'
-	capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
-	capabilities['build'] = ENV['BS_AUTOMATE_BUILD']? ENV['BS_AUTOMATE_BUILD'] : 'browserstack-build-1'
-  capabilities['browserstack.local'] = 'false'
-  capabilities['browserstack.source'] = 'cucumber-ruby:sample-master:v1.0'     
-
-  if capabilities['browserstack.local'] && capabilities['browserstack.local'] == 'true';
+  @caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][TASK_ID])
+  # Code to start browserstack local before start of test
+  if @caps['browserstack.local'] && @caps['browserstack.local'].to_s == 'true'
     @bs_local = BrowserStack::Local.new
-    bs_local_args = { "key" => "#{ENV['BS_AUTHKEY']}", "forcelocal" => true }
+    bs_local_args = { 'key' => (CONFIG['key']).to_s }
     @bs_local.start(bs_local_args)
   end
-  Capybara::Selenium::Driver.new(app, :browser => :remote, :url => url, :desired_capabilities => capabilities)
 
-
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :remote,
+                                 url: "https://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",
+                                 desired_capabilities: @caps)
 end
 
 Capybara.default_driver = :browserstack
-Capybara.app_host = "http://www.google.com"
 Capybara.run_server = false
 
+# Code to stop browserstack local after end of test
 at_exit do
-  @bs_local.stop unless @bs_local.nil? 
+  @bs_local.stop unless @bs_local.nil?
 end
